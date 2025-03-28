75b43f9db8f06baf23e5e0a6affd146f84cd7133
Kastuell <sholotun@mail.ru>
2025-03-28 23:36:17 +1000

    vercel deploy fix

--- src/entities/employee/api/employees.api.ts
+++ src/entities/employee/api/employees.api.ts
@@ -1,7 +1,12 @@
 import { EmployeeT } from "../types/employee.types";
 
+const API_URL =
+  import.meta.env.PROD == true
+    ? "https://pizzasoft-test-f0ecjrsz9-134.vercel.app/employees.json"
+    : "http://localhost:5173/public/employees.json";
+
 export const fetchEmployees = async () => {
-  const res = await fetch("http://localhost:5173/public/employees.json");
+  const res = await fetch(API_URL);
 
   if (!res.ok) {
     throw new Error("Ошибка получения данных!");
